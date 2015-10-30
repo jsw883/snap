7ecc4effedcaee93c6d6245091b2dc1c7812205b
James Williams <jsw883@gmail.com>
2015-10-30 00:26:59 -0400

    sketch of architecture in wcmty.* (nothing implemented)

--- contrib/yins-enas/examples/community/Makefile.ex
+++ contrib/yins-enas/examples/community/Makefile.ex
@@ -5,9 +5,11 @@
 MAIN = community
 DEPH = \
     $(YINSCORE)/wgraph.h \
+    $(YINSCORE)/wcmty.h \
     $(YINSCORE)/wgio.h \
     $(YINSCORE)/io.h
 DEPCPP = \
     $(YINSCORE)/wgraph.cpp \
+    $(YINSCORE)/wcmty.cpp \
     $(YINSCORE)/wgio.cpp \
     $(YINSCORE)/io.cpp
--- contrib/yins-enas/examples/community/stdafx.h
+++ contrib/yins-enas/examples/community/stdafx.h
@@ -5,5 +5,6 @@
 #include "Snap.h"
 
 #include "wgraph.h"
+#include "wcmty.h"
 #include "wgio.h"
 #include "io.h"
