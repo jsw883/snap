9d325b31c09992bdb84aa6e1c87b18358a3a0534
Addison Hu <huisaddison@users.noreply.github.com>
2015-10-29 20:42:47 -0400

    finished debugging vespignani

--- contrib/yins-enas/backbone/vespignani/vespignani.cpp
+++ contrib/yins-enas/backbone/vespignani/vespignani.cpp
@@ -15,7 +15,6 @@ int main(int argc, char* argv[]) {
   const TStr OutFNm = Env.GetIfArgPrefixStr("-o:", "", "output network name(filename extensions added)");
   const float alpha = Env.GetIfArgPrefixFlt("-a:", 0.01, "level of significance alpha");
 
-  int edgesCleared = 0;
   // Load graph and create directed and undirected graphs (pointer to the same memory)
   printf("\nLoading %s...", InFNm.CStr());
   PFltWNGraph WGraph = TSnap::LoadFltWEdgeList<TWNGraph>(InFNm);
@@ -49,72 +48,53 @@ int main(int argc, char* argv[]) {
 
   printf("\n Applying the disparity filter...");
   typename PFltWNGraph::TObj::TEdgeI EI;
-  for (EI = WGraph->BegEI(); EI < WGraph->EndEI(); ) {
+  for (EI = WGraph->BegEI(); EI < WGraph->EndEI(); EI--) {
+
   	if (WGraph->GetEdges() == 0) {
   		break;
-  	} else if (true){
-  		EI--;
-		printf("%i\n", WGraph->GetNI(EI.GetSrcNId()).GetOutDeg());
-		WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
-		printf("%i\n", WGraph->GetNI(EI.GetSrcNId()).GetOutDeg());
-		edgesCleared++;
-		printf("%i\n", edgesCleared);
-  	} else {
-  		EI++;
   	}
+    // If the edge meets the criterion for either the source or the destination,
+    // add it to the backbone graph. 
+
+    // If we were computing this on the fly, WGraph.GetNI(EI.GetSrcNId).GetOutWDeg(); replaces the 
+    // hashtable lookup for WDeg of Source Node ID
+    // This is if the calculation is not iteration independent (this command only works if
+    // we are removing edges in place.)
+
+    // Check whether this is the only out-edge for the source node
+    if ((OutDegH.GetDat(EI.GetSrcNId()) == 1) && (InDegH.GetDat(EI.GetDstNId()) > 1)) {
+      // // If so, remove edge only if not significant for destination node
+      if (!(pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)) < alpha)) {
+        WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
+      } else {
+      	EI++;
+      }
+    
+    // Check whether this is the only in-edge for the destination node
+    } else if ((OutDegH.GetDat(EI.GetSrcNId()) > 1) && (InDegH.GetDat(EI.GetDstNId()) == 1)) {
+    //   If so, remove edge only if not significant for source node
+     	if (!(pow((1 - EI.GetW() / OutWDegH.GetDat(EI.GetSrcNId())), (OutDegH.GetDat(EI.GetSrcNId()) - 1)) < alpha)) {
+        WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
+    	} else {
+      	EI++;
+      }
+    // Check whether this is not the only out-edge for source and not the only in-edge for destination
+   	} else if ((OutDegH.GetDat(EI.GetSrcNId()) > 1) && (InDegH.GetDat(EI.GetDstNId()) > 1)) {
+      // If so, remove edge only if not significant for either side.
+      if (!
+        ((pow((1 - EI.GetW() / OutWDegH.GetDat(EI.GetSrcNId())), (OutDegH.GetDat(EI.GetSrcNId()) - 1)) < alpha) || 
+                (pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)) < alpha))) {
+          	WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
+          } else {
+      			EI++;
+        }
+    	} else {
+    		EI++;
+    		// WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
+    }
+    // Implicitly, an edge is kept if it is the only out-edge for the source and only in-edge for destination
+    // Which aligns with the Vespignani paper.
   }
-
-  //   // If the edge meets the criterion for either the source or the destination,
-  //   // add it to the backbone graph. 
-
-  //   // If we were computing this on the fly, WGraph.GetNI(EI.GetSrcNId).GetOutWDeg(); replaces the 
-  //   // hashtable lookup for WDeg of Source Node ID
-  //   // This is if the calculation is not iteration independent (this command only works if
-  //   // we are removing edges in place.)
-
-  // 	// if ((pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)) < 0) ||
-  // 	// 	pow((1 - EI.GetW() / OutWDegH.GetDat(EI.GetSrcNId())), (OutDegH.GetDat(EI.GetSrcNId()) - 1)) < 0){
-  // 	// 	neg++;
-  // 	// } 
-
-  //   // Check whether this is the only out-edge for the source node
-  //   if ((OutDegH.GetDat(EI.GetSrcNId()) == 1) && (InDegH.GetDat(EI.GetDstNId()) > 1)) {
-  //     // If so, remove edge only if not significant for destination node
-  //     if (!(pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)) < alpha)) {
-  //       // printf("%f\n", pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)));
-  //       WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
-  //     } else {
-  //     	printf("%f\n", pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)));
-  //     }
-  //   }
-  //   // Check whether this is the only in-edge for the destination node
-  //   else if ((OutDegH.GetDat(EI.GetSrcNId()) > 1) && (InDegH.GetDat(EI.GetDstNId()) == 1)) {
-  //     // If so, remove edge only if not significant for source node
-  //     if (!(pow((1 - EI.GetW() / OutWDegH.GetDat(EI.GetSrcNId())), (OutDegH.GetDat(EI.GetSrcNId()) - 1)) < alpha)) {
-  //       WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
-  //   } else {
-  //     	printf("%f\n", pow((1 - EI.GetW() / OutWDegH.GetDat(EI.GetSrcNId())), (OutDegH.GetDat(EI.GetSrcNId()) - 1)));
-  //     }
-  //   }
-  //   // Check whether this is not the only out-edge for source and not the only in-edge for destination
-  //   else if ((OutDegH.GetDat(EI.GetSrcNId()) > 1) && (InDegH.GetDat(EI.GetDstNId()) > 1)) {
-  //     // If so, remove edge only if not significant for either side.
-  //     if (!
-  //       ((pow((1 - EI.GetW() / OutWDegH.GetDat(EI.GetSrcNId())), (OutDegH.GetDat(EI.GetSrcNId()) - 1)) < alpha) || 
-  //               (pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)) < alpha))
-  //       ) {
-  //         	WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
-  //         } else {
-  //     		printf("%f\n", pow((1 - EI.GetW() / OutWDegH.GetDat(EI.GetSrcNId())), (OutDegH.GetDat(EI.GetSrcNId()) - 1)));
-  //     		printf("%f\n", pow((1 - EI.GetW() / InWDegH.GetDat(EI.GetDstNId())), (InDegH.GetDat(EI.GetDstNId()) - 1)));
-          	
-  //         }
-  //   } else {
-  //   	WGraph->DelEdge(EI.GetSrcNId(), EI.GetDstNId());
-  //   }
-  //   // Implicitly, an edge is kept if it is the only out-edge for the source and only in-edge for destination
-  //   // Which aligns with the Vespignani paper.
-  // }
   printf(" DONE (time elapsed: %s (%s))\n", ExeTm.GetTmStr(), TSecTm::GetCurTm().GetTmStr().CStr());
 
   // OUTPUTTING 
@@ -124,7 +104,7 @@ int main(int argc, char* argv[]) {
   printf("  edges: %d\n", WGraph->GetEdges());
   
   printf("\nSaving...");
-  TSnap::SaveFltWEdgeList(WGraph, TStr::Fmt("%s-%.10f", OutFNm.CStr(), alpha), "");
+  TSnap::SaveFltWEdgeList(WGraph, TStr::Fmt("%s-%f.snap", OutFNm.CStr(), alpha), "");
   printf(" DONE\n");
 
   
