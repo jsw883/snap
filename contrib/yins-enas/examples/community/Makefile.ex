e736b1b718e3b3cecaa4c29af613d0f4291af345
James Williams <jsw883@gmail.com>
2015-10-30 00:16:38 -0400

    started community example (initial scaffolding)

---
+++ contrib/yins-enas/examples/community/Makefile.ex
@@ -0,0 +1,13 @@
+#
+#	configuration variables for the example
+#
+
+MAIN = community
+DEPH = \
+    $(YINSCORE)/wgraph.h \
+    $(YINSCORE)/wgio.h \
+    $(YINSCORE)/io.h
+DEPCPP = \
+    $(YINSCORE)/wgraph.cpp \
+    $(YINSCORE)/wgio.cpp \
+    $(YINSCORE)/io.cpp
---
+++ contrib/yins-enas/examples/community/stdafx.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "targetver.h"
+
+#include "Snap.h"
+
+#include "wgraph.h"
+#include "wgio.h"
+#include "io.h"
