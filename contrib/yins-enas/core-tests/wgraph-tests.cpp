ce39720a9044717aaf8f460da1cb1c38d2b5aac1
James Williams <jsw883@gmail.com>
2015-11-04 17:05:38 -0500

    implemented degree testing (everything seems okay)

--- contrib/yins-enas/core-tests/wgraph-tests.cpp
+++ contrib/yins-enas/core-tests/wgraph-tests.cpp
@@ -64,44 +64,80 @@ TYPED_TEST(GraphTest, GeneralGraphFunctionality) {
   int Nodes = 10000;
   int Edges = 1000000;
 
-  int i, counter;
+  int counter;
   int SrcNId, DstNId;
 
-  TEdgeW W = 1;
+  int InDeg, OutDeg, Deg;
+  int TotalInDeg = 0, TotalOutDeg = 0, TotalDeg = 0;  
+  TEdgeW WInDeg, WOutDeg, WDeg;
+  TEdgeW TotalWInDeg = 0, TotalWOutDeg = 0, TotalWDeg = 0;  
+
+  TEdgeW W, EdgeW;
+  TEdgeW MxW = 100, TotalW = 0;
 
   EXPECT_TRUE(Graph->Empty());
 
   // CREATE NODES AND EDGES
 
   // create nodes
-  for (i = 0; i < Nodes; i++) {
-    Graph->AddNode(i);
+  for (counter = 0; counter < Nodes; counter++) {
+    Graph->AddNode(counter);
   }
+  // graph properties
   EXPECT_FALSE(Graph->Empty());
   EXPECT_TRUE(Graph->IsOk());
+  // count nodes
   EXPECT_EQ(Nodes, Graph->GetNodes());
 
   // create edges (unique)
-  for (i = 0; i < Edges; ) {
+  for (counter = 0; counter < Edges; ) {
     SrcNId = (long) (Nodes * drand48());
     DstNId = (long) (Nodes * drand48());
+    W = (TEdgeW) (MxW * drand48() + 1);
     if (SrcNId != DstNId  &&  !Graph->IsEdge(SrcNId, DstNId)) {
       Graph->AddEdge(SrcNId, DstNId, W);
-      i++;
+      counter++;
+      // weights
+      TotalW += W;
     }
   }
+  // graph properties
   EXPECT_FALSE(Graph->Empty());
   EXPECT_TRUE(Graph->IsOk());
+  // count edges
   EXPECT_EQ(Edges, Graph->GetEdges());
+  // weights
+  EXPECT_FLOAT_EQ(TotalW, Graph->GetTotalW());
 
   // NODE ITERATORS
 
   // nodes forward iterator
   counter = 0;
   for (NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
-    counter++;
+    // check node existence
     EXPECT_TRUE(Graph->IsNode(NI.GetId()));
+    // degrees
+    InDeg = NI.GetInDeg(); TotalInDeg += InDeg;
+    OutDeg = NI.GetOutDeg(); TotalOutDeg += OutDeg;
+    Deg = NI.GetDeg(); TotalDeg += Deg;
+    EXPECT_EQ(Deg, InDeg + OutDeg);
+    // weighted degrees
+    WInDeg = NI.GetWInDeg(); TotalWInDeg += WInDeg;
+    WOutDeg = NI.GetWOutDeg(); TotalWOutDeg += WOutDeg;
+    WDeg = NI.GetWDeg(); TotalWDeg += WDeg;
+    EXPECT_EQ(WDeg, WInDeg + WOutDeg);
+    // count nodes
+    counter++;
   }
+  // degrees
+  EXPECT_FLOAT_EQ(Edges, TotalInDeg);
+  EXPECT_FLOAT_EQ(Edges, TotalOutDeg);
+  EXPECT_FLOAT_EQ(Edges, TotalDeg/2);
+  // weighted degrees
+  EXPECT_FLOAT_EQ(TotalW, TotalWInDeg);
+  EXPECT_FLOAT_EQ(TotalW, TotalWOutDeg);
+  EXPECT_FLOAT_EQ(TotalW, TotalWDeg/2);
+  // count nodes
   EXPECT_EQ(Nodes, counter);
 
   // nodes backward iterator
@@ -164,45 +200,59 @@ TYPED_TEST(TWNGraphTest, SpecificGraphFunctionality) {
     DstNId = (long) (Nodes * drand48());
     W = (TEdgeW) (MxW * drand48() + 1);
     if (SrcNId != DstNId  &&  !Graph->IsEdge(SrcNId, DstNId)) {
+      // create edge
       Graph->AddEdge(SrcNId, DstNId, W);
+      counter++;
+      // weights
       TotalW += W;
       EXPECT_DOUBLE_EQ(W, Graph->GetEI(SrcNId, DstNId).GetW());
-      counter++;
     }
   }
-  EXPECT_FLOAT_EQ(TotalW, Graph->GetTotalW());
 
   // EDGE ITERATORS
 
   // edges forward iterator
   counter = 0;
   for (EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
+    // weights
     EdgeTotalW += EI.GetW();
-    counter++;
+    // edge existence
     EXPECT_TRUE(Graph->IsEdge(EI.GetSrcNId(), EI.GetDstNId()));
+    // count edges
+    counter++;
   }
+  // count edges
   EXPECT_EQ(Edges, counter);
+  // weights
   EXPECT_FLOAT_EQ(TotalW, EdgeTotalW);
 
   // edges backward iterator
   counter = 0;
   for (EI = Graph->EndEI(); EI > Graph->BegEI(); ) {
     EI--;
-    counter++;
+    // edge existence
     EXPECT_TRUE(Graph->IsEdge(EI.GetSrcNId(), EI.GetDstNId()));
+    // count edges
+    counter++;
   }
+  // count edges
   EXPECT_EQ(Edges, counter);
 
-  // count edges (per node)
+  // edges (per node)
   counter = 0;
   for (NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
     for (int e = 0; e < NI.GetOutDeg(); e++) {
+      // weights
       NodeTotalW += NI.GetOutEW(e);
-      counter++;
+      // edge existence
       EXPECT_TRUE(Graph->IsEdge(NI.GetId(), NI.GetOutNId(e)));
+      // count edges
+      counter++;
     }
   }
+  // count edges
   EXPECT_EQ(Edges, counter);
+  // weights
   EXPECT_FLOAT_EQ(TotalW, NodeTotalW);
 
 }
@@ -256,51 +306,65 @@ TYPED_TEST(TWNEGraphTest, SpecificGraphFunctionality) {
     DstNId = (long) (Nodes * drand48());
     W = (TEdgeW) (MxW * drand48() + 1);
     if (SrcNId != DstNId  &&  !Graph->IsEdge(SrcNId, DstNId)) {
+      // create edge
       EId = Graph->AddEdge(SrcNId, DstNId, W);
+      counter++;
+      // weights
       TotalW += W;
       EXPECT_DOUBLE_EQ(W, Graph->GetEI(EId).GetW());
-      counter++;
     }
   }
-  EXPECT_FLOAT_EQ(TotalW, Graph->GetTotalW());
 
   // EDGE ITERATORS
 
   // edges forward iterator
   counter = 0;
   for (EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
+    // weights
     EdgeTotalW += EI.GetW();
-    counter++;
+    // check edge existence
     EXPECT_TRUE(Graph->IsEdge(EI.GetId()));
+    // count edges
+    counter++;
   }
+  // count edges
   EXPECT_EQ(Edges, counter);
+  // weights
   EXPECT_FLOAT_EQ(TotalW, EdgeTotalW);
 
   // edges backward iterator
   counter = 0;
   for (EI = Graph->EndEI(); EI > Graph->BegEI(); ) {
     EI--;
-    counter++;
+    // edge existence
     EXPECT_TRUE(Graph->IsEdge(EI.GetId()));
+    // count edges
+    counter++;
   }
+  // count edges
   EXPECT_EQ(Edges, counter);
 
-  // count edges (per node)
+  // edges (per node)
   counter = 0;
   for (NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
     for (int e = 0; e < NI.GetOutDeg(); e++) {
+      // weights
       NodeTotalW += NI.GetOutEW(e);
-      counter++;
+      // check edge existence
       EXPECT_TRUE(Graph->IsEdge(NI.GetOutEId(e)));
+      // count edges
+      counter++;
     }
   }
+  // count edges
   EXPECT_EQ(Edges, counter);
+  // weights
   EXPECT_FLOAT_EQ(TotalW, NodeTotalW);
 
 }
 
   // // create edges
-  // for (i = 0; i < Edges; i++) {
+  // for (counter = 0; counter < Edges; counter++) {
   //   SrcNId = (long) (Nodes * drand48());
   //   DstNId = (long) (Nodes * drand48());
   //   while (SrcNId == DstNId) {
