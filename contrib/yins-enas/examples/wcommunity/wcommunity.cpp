fd117a5ae21c190e05aea730b71dac6f033afd1b
James Williams <jsw883@gmail.com>
2016-12-19 13:17:09 +1300

    wcommunity outputs subgraphs

--- contrib/yins-enas/examples/wcommunity/wcommunity.cpp
+++ contrib/yins-enas/examples/wcommunity/wcommunity.cpp
@@ -13,10 +13,12 @@ int main(int argc, char* argv[]) {
   
   const TStr InFNm = Env.GetIfArgPrefixStr("-i:", "", "input network (tab separated list of edges with edge weights)");
   const TStr OutFNm = Env.GetIfArgPrefixStr("-o:", "", "output prefix (filename extensions added)");
+  const TStr OutDir = OutFNm.LeftOfLast('/');
   const TStr BseFNm = OutFNm.RightOfLast('/');
   const double eps = Env.GetIfArgPrefixFlt("--eps:", 1.0e-5, "minimum quality improvement threshold");
   const double moves = Env.GetIfArgPrefixFlt("--moves:", 1.0e-2, "minimum number of moves (relative)");
   const int iters = Env.GetIfArgPrefixInt("--iters:", 1.0e+4, "maximum number of iterations");
+  const bool subgraphs = Env.GetIfArgPrefixBool("--subgraphs:", false, "output community subgraphs: T / F");
   
   const double S = Env.GetIfArgPrefixFlt("-s:", 1.0, "community vertex color saturation value (0.0 - 1.0)");
   const double L = Env.GetIfArgPrefixFlt("-l:", 0.5, "community vertex lightness value (0.0 - 1.0)");
@@ -29,12 +31,13 @@ int main(int argc, char* argv[]) {
   TSnap::printFltWGraphSummary(WGraph, true, "\nWGraph\n------");
   
   // Declare variables
-  TIntIntVH NIdCmtyVH;
+  TIntIntVH NIdCmtyVH, CmtyNIdVH;
   int NCmty;
   double LouvainQ;
 
-  TStr Name;
+  PFltWNGraph WSubGraph;
 
+  TStr Name;
   TStr method = "louvain";
 
   // COMMUNITY
@@ -77,6 +80,37 @@ int main(int argc, char* argv[]) {
   TSnap::SaveTxt(NIdHexH, Name.CStr(), "Louvain modularity final community hierarchy coloring (weighted)", "NodeId", "Col");
   printf(" DONE\n");
   
+  if (subgraphs) {
+
+    printf("0\n");
+
+    // Separate nodes into communities
+    // for (int i = 0; i < NCmty; i++) {
+    //   CmtyNIdVH.AddKey(NCmty);
+    // }
+    for (HI = NIdCmtyVH.BegI(); HI < NIdCmtyVH.EndI(); HI++) {
+      const int& CmtyId = HI.GetDat().Last();
+      const int& NId = HI.GetKey();
+      if (!CmtyNIdVH.IsKey(CmtyId)) {
+        CmtyNIdVH.AddDat(CmtyId, TIntV::GetV(NId));
+      } else {
+        CmtyNIdVH.GetDat(CmtyId).Add(NId);
+      }
+    }
+
+    printf("1\n");
+
+    for (HI = CmtyNIdVH.BegI(); HI < CmtyNIdVH.EndI(); HI++) {
+      const int& CmtyId = HI.GetKey();
+      const TIntV& NIdV = HI.GetDat();
+      WSubGraph = TSnap::GetSubGraph(WGraph, NIdV);
+      TSnap::SaveFltWEdgeList(WSubGraph, TStr::Fmt("%s/%04d.snap", OutDir.CStr(), CmtyId), TStr::Fmt("Wcommunity: %04d", CmtyId));
+    }
+
+    printf("2\n");
+
+  }
+
   Catch
   
   printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(), TSecTm::GetCurTm().GetTmStr().CStr());
